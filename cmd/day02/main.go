680d410303a3fc79d68fc9df366f290a0923e190
Marc Schuba <marc.schuba@care.com>
2023-12-02 16:37:16 +0100

    WIP day 2

---
+++ cmd/day02/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(V1())
+	// fmt.Println(V2())
+}
+
+func V1() int {
+	
+	return 0
+}
+
+func processInput(input string) int {
+	for _, line := range strings.Split(input, "\n") {
+
+	}
+	return 8
+}
